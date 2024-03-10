c65744d248623322512924301c77a6b05399c0b5
aniki219 <aniki219@gmail.com>
2024-03-09 22:31:17 -0800

    tiled imports

---
+++ Assets/Tiled/Tilemaps/WallTilesSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="WallTilesSheet" tilewidth="24" tileheight="24" tilecount="55" columns="11">
+ <image source="../WallTilesSheet.png" width="264" height="120"/>
+</tileset>
---
+++ Assets/Tiled/Tilemaps/lasers.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="lasers" tilewidth="24" tileheight="24" tilecount="18" columns="6" tilerendersize="grid">
+ <image source="../../Sprites/LaserSheet.png" width="144" height="72"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
