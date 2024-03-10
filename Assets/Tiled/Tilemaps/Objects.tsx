9dd684e0d5fcc391a4d93d17333014be745d186d
aniki219 <aniki219@gmail.com>
2024-03-09 23:14:34 -0800

    prefab importer

--- Assets/Tiled/Tilemaps/Objects.tsx
+++ Assets/Tiled/Tilemaps/Objects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="24" tileheight="24" tilecount="80" columns="8" tilerendersize="grid">
+<tileset version="1.10" tiledversion="1.10.2" name="Objects" tilewidth="24" tileheight="24" tilecount="80" columns="8" objectalignment="center" tilerendersize="grid">
  <image source="../Objects.png" width="192" height="240"/>
 </tileset>
